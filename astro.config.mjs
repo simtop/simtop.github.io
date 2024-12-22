72dee0f501ca349e5cb155ed9312f6af46441470
simtop <41802904+simtop@users.noreply.github.com>
2024-12-22 13:12:33 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'simtop.github.io',
+    site: 'https://simtop.github.io',
     integrations: [
         mdx(),
         sitemap(),
